aa5862a7ff24feffdb1ac14e3b7b2fc9aa0efde0
Пользователь <Valenta2910@mail.ru>
2023-07-05 23:06:03 +0300

    initial commit

--- src/main/java/pro/sky/java/course2/hw_spring_boot/service/EmployeeServiceImpl.java
+++ src/main/java/pro/sky/java/course2/hw_spring_boot/service/EmployeeServiceImpl.java
@@ -2,6 +2,8 @@ package pro.sky.java.course2.hw_spring_boot.service;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -20,33 +22,38 @@ public class EmployeeServiceImpl implements EmployeeService {
 
 
     private final EmployeeRepository employeeRepository;
-
+    private static final Logger logger = LoggerFactory.getLogger(EmployeeService.class);
     public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
         this.employeeRepository = employeeRepository;
     }
 
     @Override
     public List<Employee> getAllEmployees() {
+        logger.debug("List of all employees");
         return employeeRepository.getAllEmployees();
     }
 
     @Override
     public Double getSalarySum() {
+        logger.debug("Sum of salary");
         return employeeRepository.getSalarySum();
     }
 
     @Override
     public Optional<Integer> getMinSalary() {
+        logger.debug("Employee with min salary");
         return employeeRepository.getMinSalary();
     }
 
     @Override
     public List<Employee> getMaxSalary() {
+        logger.debug("Employee with max salary");
         return employeeRepository.getMaxSalary();
     }
 
     @Override
     public List<Employee> getAllEmployeesWithSalaryHigherThenAvg() {
+        logger.debug("Employees with salary higher then average");
         return employeeRepository.getAllEmployees().stream()
                 .filter(employee -> employee.getSalary() > getSalarySum() / employeeRepository.getAllEmployees().size())
                 .toList();
@@ -54,18 +61,20 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     @Override
     public List<Employee> addEmployee(Employee employee) {
+        logger.debug("Employee added " + employee);
         return (List<Employee>) employeeRepository.save(employee);
     }
 
 
     @Override
     public List<Employee> updateEmployee(Employee employee) {
+        logger.debug("Employee updated " + employee);
         return Collections.singletonList(employeeRepository.save(employee));
     }
 
     @Override
     public List<EmployeeDTO> getEmployeeById(int id) {
-
+        logger.debug("Employee with id= "+id);
         return employeeRepository.findById(id).stream()
                 .map(EmployeeDTO::fromEmployee)
                 .collect(Collectors.toList());
@@ -73,10 +82,12 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Override
     public void deleteEmployeeById(int id) {
         employeeRepository.deleteById(id);
+        logger.debug("Deleted employee with id= "+id);
     }
 
     @Override
     public List<EmployeeDTO> getAllEmployeesWithSalaryHigherThan(int salary) {
+        logger.debug("List of employees with salary higher than "+salary);
         return employeeRepository.getAllEmployeesWithSalaryHigherThan(salary).stream()
                 .map(EmployeeDTO::fromEmployee)
                 .collect(Collectors.toList());
@@ -84,6 +95,7 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     @Override
     public List<EmployeeDTO> getAllEmployeesWithMatchingPosition(String position) {
+        logger.debug("List of employees with position "+position);
         return employeeRepository.getAllEmployeesWithMatchingPosition(position).stream()
                 .map(EmployeeDTO::fromEmployee)
                 .collect(Collectors.toList());
@@ -91,6 +103,7 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     @Override
     public List<EmployeeDTO> getEmployeeFullInfo(int id) {
+        logger.debug("Full information about employee with id= "+id);
         return employeeRepository.findById(id).stream()
                 .map(EmployeeDTO::fromEmployee)
                 .collect(Collectors.toList());
@@ -111,6 +124,7 @@ public class EmployeeServiceImpl implements EmployeeService {
         List<Employee> employees = objectMapper.readValue(file.getBytes(), new TypeReference<>() {
         });
         employeeRepository.saveAll(employees);
+        logger.debug("Employees added");
     }
 
 
